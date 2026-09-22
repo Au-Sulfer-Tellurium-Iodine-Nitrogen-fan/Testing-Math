04f6a4b00e19e41c543894482e6778b95eb5fa3c
Au-Sulfer-Tellurium-Iodine-Nitrogen-fan <128Mleotta@frhsd.com>
2026-09-22 14:50:00 -0400

    Tests  reciptricols, multiplying, dividing and nested powers

--- src/MathTest.java
+++ src/MathTest.java
@@ -1,11 +1,142 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static java.lang.Math.negateExact;
+import static java.lang.Math.pow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MathTest {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5",
+            "1.5, 3.7",
+            "-3.4, -5.2",
+            "-2, -4",
+            "5, -6",
+            "3.5, -4.3",
+            "-4, 6",
+            "-6.4, 5.98",
+            "0, 0",
+            "0, -1.4",
+            "0, -4",
+            "0, 1.4",
+            "0, 4",
+            "1, 0",
+            "1, -1.4",
+            "1, -4",
+            "1, 1.4",
+            "1, 4",
+            "-1, 0",
+            "-1, -1.4",
+            "-1, -4",
+            "-1, 1.4",
+            "-1, 4",
+            "0, 1",
+            "-1.4, 1",
+            "-4, 1",
+            "1.4, 1",
+            "4, 1"
+    })
+    public void testReciprocal(double base, double exponent) {
+        Assertions.assertEquals(1 / pow(base, exponent),
+                pow(base, exponent), "base to the power of -a is equal to 1/base to the power of a");
     }
+        // need to make exponent negative
+
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testMultiplying(double base, double a, double b) {
+        Assertions.assertEquals(pow(base,a*b),
+        pow(base,a) * pow(base,b),
+                "base to the power of a times base to te power of b should equal 8");
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testDividing(double base, double a, double b) {
+        Assertions.assertEquals(pow(base,a-b),
+                pow(base,a) / pow(base,b),
+                "base to the power of a divided by base to the power of b is equal to base to the power of a-b");
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "6, 5, 17",
+            "5, 2, -3",
+            "3, -3, -4",
+            "-7, 2, 4",
+            "-4, -5, 2",
+            "-7, -8, -9",
+            "6.3, 5.5, 17.3",
+            "5.25, 2.4, -3.2",
+            "3.4, -3.3, -4.2",
+            "-7.5, 2.2, 4.6",
+            "-4.3, -5.2, 2.1",
+            "-7.4, -8.3, -9.2"
+    })
+    public void testNestedPowers(double base, double a, double b) {
+        double hi = pow(base,a);
+        Assertions.assertEquals(pow(base, a*b),
+                pow(hi,b),
+                "base to the power of a to the power of b is equal to x to the power of a*b");
+
+
+
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
+    public void testZeroPower(double base) {
+        x0 = 1
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
+    public void testOnePower(double base) {
+        x1 = x
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
+    public void testZeroBase(double exponent) {
+        0x = 0
+    }
+
+
 }
