63b25e8e5b914906c5b6ea5d7dfbcb096243fe79
Au-Sulfer-Tellurium-Iodine-Nitrogen-fan <128Mleotta@frhsd.com>
2026-09-22 14:52:48 -0400

    test zero power and test one power

--- src/MathTest.java
+++ src/MathTest.java
@@ -121,14 +121,19 @@ public class MathTest {
     @ParameterizedTest
     @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
     public void testZeroPower(double base) {
-        x0 = 1
+        Assertions.assertEquals(1,
+                pow(base,0),
+                "base to the power of 0 is equal to 1");
+
     }
 
 
     @ParameterizedTest
     @ValueSource(doubles = {1, 5.0, 3.7, -1, -4.0, -4.9})
     public void testOnePower(double base) {
-        x1 = x
+        Assertions.assertEquals(base,
+                pow(base,1),
+                "Base to the power of 1 is equal to base");
     }
 
 
